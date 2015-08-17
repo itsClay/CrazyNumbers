7bbdfdecce5d11826028bec698d1480f2c589db2
Clay Shaw <Claydshaw@gmail.com>
2015-08-17 11:04:33 -0700

    complete ajax calls for data use

--- CrazyNumbersApp/static/CrazyNumbersApp/js/bar_chart.js
+++ CrazyNumbersApp/static/CrazyNumbersApp/js/bar_chart.js
@@ -1,25 +1,32 @@
 $(document).ready(function(){
 
+// Arbitrary Data for trials
 var data = [4, 8, 15, 16, 23, 42];
 
-var userData2 = [
-    {
-        "User_id": 2,
-        "Number": "415-279-5665",
-        "DateTime_created": "2015-08-11T22:36:05Z"
-    },
-    {
-        "User_id": 3,
-        "Number": "555-555-5555",
-        "DateTime_created": "2015-08-11T22:39:07Z"
-    }
-];
+// create variable for storage of our data
+var userData,
+	domainTest;
 
-console.log(userData2.length)
-
-// json call to our api in d3
-var userData = d3.json('http://localhost:8000/api/users/')
-
+// json call to our api
+var getUserdata = function(){	
+	return $.ajax({
+		url: 'http://localhost:8000/api/users/',
+		type: 'GET',
+		dataType: 'json',
+		success:function(data){
+			assignUserdata(data);
+		}
+	})
+};
+// callback to handle our result
+var assignUserdata = function(data){
+	userData = data
+	console.log(data)
+	domainTest = [0, d3.max(data)]
+	console.log(domainTest)
+};
+// execute
+getUserdata()
 
 // bar dimensions
 var width = 500,
@@ -28,17 +35,17 @@ var width = 500,
 
 // set scalable data numbers to fit
 var xRange = d3.scale.linear()
-	.domain([0, d3.max(userData2)])
+	.domain([0, d3.max(data)])
 	.range([0, width]);
 
 // dynamic chart size
 var chart = d3.select('#barSVG')
 	.attr('width', width)
-	.attr('height', barHeight * userData2.length);
+	.attr('height', barHeight * data.length);
 
 // create bars
 var bar = chart.selectAll('g')
-	.data(userData2.User_id)
+	.data(data)
 	.enter().append('g')
 	.attr('transform', function(d, i){
 		return "translate(0," + i * barHeight + ")"
--- CrazyNumbersApp/static/CrazyNumbersApp/js/main.js
+++ CrazyNumbersApp/static/CrazyNumbersApp/js/main.js
@@ -3,14 +3,11 @@ $(document).ready(function(){
 
 	var userData = []
 
-	var userGet = $.ajax({
+var userGet = $.ajax({
 		url: 'http://localhost:8000/api/users/',
 		type: 'GET',
 		dataType: 'json',
 		success: function(data){
-			for(var obj in data){
-				userData.push(data[obj])
-			}
 		}
 	})
 })
